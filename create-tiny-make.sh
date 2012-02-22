2f4312a2d2d943417de0d70a7b74f0f9e41d8306
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2012-02-22 11:41:57 +0900

    Enable quick make on current tiny-make
    We couldn't try quick "make" on current tiny-make
    after "git clone" since it required some settings
    which was rename/re-settings and so on.
    
    it still require sample.config to .config,
    sample.sys.config to sys.config.
    but it is easy
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- create-tiny-make.sh
+++ create-tiny-make.sh
@@ -5,8 +5,12 @@
 #
 #===============================
 TNAME=`head -n 1 Makefile | cut -d' ' -f2`
-TLIST=" script\
+TLIST=" script/Makefile.build\
+	script/Makefile.clean\
+	script/Makefile.common\
+	script/Makefile.depend\
 	Makefile\
+	sample.config\
 	sample.sys.config\
 	"
 
